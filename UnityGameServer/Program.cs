395920f3d44396749b6f9e9d5c5510431b9f6acb
ghuijsmans <huijsmansgijs@gmail.com>
2021-01-15 00:42:59 +0100

    Add project files.

---
+++ UnityGameServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UnityGameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Server Window";
+            Server.Start(50, 26950);
+            Console.ReadKey();
+        }
+    }
+}
